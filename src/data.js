dd3408a5149c48c3ac0105e8f717f76be7325c68
banicaDaniel07 <banica.daniel07@gmail.com>
2021-06-12 13:48:23 +0300

    Added JSON file

--- src/data.js
+++ src/data.js
@@ -1,109 +1,416 @@
 const DATA_JSON = [
     {
-   "id":1,
-   "name":"Mulan Landing Page",
-   "responsive":false,
-   "website":"https://mulan-landing-page-d.netlify.app/",
-   "repository":"https://github.com/banicaDaniel07/mulan-landing-page",
-   "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
-   "code":{
-      "HTML":true,
-      "CSS":true,
-      "JS":true,
-      "REACT":false,
-      "SASS":false
-   }
-},
+      "id":1,
+      "name":"Crwn Clothing",
+      "responsive":true,
+      "website":"https://crwn-clothing-d.herokuapp.com/",
+      "repository":"https://github.com/banicaDaniel07/crwn",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":false,
+         "JS":true,
+         "SASS":false,
+         "REACT":true
+      }
+   },
     {
-   "id":2,
-   "name":"Mulan Landing Page",
-   "responsive":true,
-   "website":"https://mulan-landing-page-d.netlify.app/",
-   "repository":"https://github.com/banicaDaniel07/mulan-landing-page",
-   "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
-   "code":{
-      "HTML":true,
-      "CSS":true,
-      "JS":true,
-      "REACT":false,
-      "SASS":false
-   } 
-},
-    {
-   "id":3,
-   "name":"Mulan Landing Page",
-   "responsive":false,
-   "website":"https://mulan-landing-page-d.netlify.app/",
-   "repository":"https://github.com/banicaDaniel07/mulan-landing-page",
-   "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
-   "code":{
-      "HTML":true,
-      "CSS":true,
-      "JS":true,
-      "REACT":false,
-      "SASS":false
-   } 
-},
-    {
-   "id":4,
-   "name":"Mulan Landing Page",
-   "responsive":false,
-   "website":"https://mulan-landing-page-d.netlify.app/",
-   "repository":"https://github.com/banicaDaniel07/mulan-landing-page",
-   "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
-   "code":{
-      "HTML":true,
-      "CSS":true,
-      "JS":true,
-      "REACT":false,
-      "SASS":false
-   } 
-},
-    {
-   "id":5,
-   "name":"Mulan Landing Page",
-   "responsive":false,
-   "website":"https://mulan-landing-page-d.netlify.app/",
-   "repository":"https://github.com/banicaDaniel07/mulan-landing-page",
-   "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
-   "code":{
-      "HTML":true,
-      "CSS":true,
-      "JS":true,
-      "REACT":false,
-      "SASS":false
-   } 
-},
-    {
-   "id":6,
-   "name":"Mulan Landing Page",
-   "responsive":false,
-   "website":"https://mulan-landing-page-d.netlify.app/",
-   "repository":"https://github.com/banicaDaniel07/mulan-landing-page",
-   "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
-   "code":{
-      "HTML":true,
-      "CSS":true,
-      "JS":true,
-      "REACT":false,
-      "SASS":false
-   } 
-},
-    {
-   "id":7,
-   "name":"Mulan Landing Page",
-   "responsive":false,
-   "website":"https://mulan-landing-page-d.netlify.app/",
-   "repository":"https://github.com/banicaDaniel07/mulan-landing-page",
-   "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
-   "code":{
-      "HTML":true,
-      "CSS":true,
-      "JS":true,
-      "REACT":false,
-      "SASS":false
-   } 
-},
+      "id":2,
+      "name":"Forkify | Recipe App",
+      "responsive":false,
+      "website":"https://forkify-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/forkify",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":false,
+         "JS":true,
+         "SASS":true,
+         "REACT":false
+      }
+   },
+   {
+      "id":3,
+      "name":"Bankist",
+      "responsive":false,
+      "website":"https://bankist-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/bankist",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":true,
+         "JS":true,
+         "REACT":false,
+         "SASS":false
+      }
+   },
+   {
+      "id":4,
+      "name":"Monster Rolodex",
+      "responsive":false,
+      "website":"https://monsters-rolodex-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/monsters-rolodex",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":true,
+         "JS":true,
+         "REACT":true,
+         "SASS":false
+      }
+   },
+   {
+      "id":5,
+      "name":"First To 100",
+      "responsive":true,
+      "website":"https://first-to-100-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/pig-game",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":true,
+         "JS":true,
+         "REACT":false,
+         "SASS":false
+      }
+   },
+   {
+      "id":6,
+      "name":"Honor | smartwatch",
+      "responsive":true,
+      "website":"https://honor-landing-page-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/honor-landing-page",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":true,
+         "JS":true,
+         "REACT":false,
+         "SASS":false
+      }
+   },
+   {
+      "id":7,
+      "name":"Comfy House",
+      "responsive":true,
+      "website":"https://comfy-house-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/comfy-house",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":false,
+         "JS":true,
+         "REACT":false,
+         "SASS":true
+      }
+   },
+   {
+      "id":8,
+      "name":"Bee IT",
+      "responsive":true,
+      "website":"https://bee-it-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/calculator",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":true,
+         "JS":true,
+         "REACT":false,
+         "SASS":false
+      }
+   },
+   {
+      "id":9,
+      "name":"Guess My Number",
+      "responsive":true,
+      "website":"https://guess-my-number-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/guess-my-number",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":true,
+         "JS":true,
+         "REACT":false,
+         "SASS":false
+      }
+   },
+   {
+      "id":10,
+      "name":"Netflix Landing Page",
+      "responsive":true,
+      "website":"https://netflix-landing-page-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/netflix-landing-page",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":true,
+         "JS":true,
+         "REACT":false,
+         "SASS":false
+      }
+   },
+   
+   {
+      "id":11,
+      "name":"Motion Capture",
+      "responsive":true,
+      "website":"https://capture-motion-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/capture-motion",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":false,
+         "JS":true,
+         "REACT":true,
+         "SASS":false
+      }
+   },
+   {
+      "id":12,
+      "name":"Bankist Arrays",
+      "responsive":false,
+      "website":"https://bankist-arrays-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/bankist-arrays",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":true,
+         "JS":true,
+         "REACT":false,
+         "SASS":false
+      }
+   },
+   {
+      "id":13,
+      "name":"Mapty",
+      "responsive":false,
+      "website":"https://mapty-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/mapty",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":true,
+         "JS":true,
+         "REACT":false,
+         "SASS":false
+      }
+   },
+   {
+      "id":14,
+      "name":"Mulan Landing Page",
+      "responsive":false,
+      "website":"https://mulan-landing-page-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/mulan-landing-page",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":true,
+         "JS":false,
+         "REACT":false,
+         "SASS":false
+      }
+   },
+   {
+      "id":15,
+      "name":"React Landing Page",
+      "responsive":true,
+      "website":"https://react-landing-page-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/react-landing-page",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":false,
+         "JS":true,
+         "REACT":true,
+         "SASS":true
+      }
+   },
+   
+   {
+      "id":16,
+      "name":"Waves Music",
+      "responsive":true,
+      "website":"https://waves-music-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/waves-music",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":false,
+         "JS":false,
+         "REACT":true,
+         "SASS":true
+      }
+   },
+   {
+      "id":17,
+      "name":"Client Side Validation",
+      "responsive":true,
+      "website":"https://client-site-validation-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/client-site-validation",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":true,
+         "JS":true,
+         "REACT":false,
+         "SASS":false
+      }
+   },
+   {
+      "id":18,
+      "name":"My First Responsive Web Page",
+      "responsive":true,
+      "website":"https://mmtuts-web-page-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/mmtuts-web-page",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":true,
+         "JS":false,
+         "REACT":false,
+         "SASS":false
+      }
+   },
+   {
+      "id":19,
+      "name":"Winter Collections",
+      "responsive":false,
+      "website":"https://winter-collections-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/winter-web",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":true,
+         "JS":false,
+         "REACT":false,
+         "SASS":false
+      }
+   },
+   {
+      "id":20,
+      "name":"Animated Bubble Page",
+      "responsive":false,
+      "website":"https://bubble-web-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/bubble-web",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":true,
+         "JS":false,
+         "REACT":false,
+         "SASS":false
+      }
+   },
+   {
+      "id":21,
+      "name":"My Gallery",
+      "responsive":false,
+      "website":"https://my-gallery-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/my-gallery",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":true,
+         "JS":true,
+         "REACT":false,
+         "SASS":false
+      }
+   },
+   {
+      "id":22,
+      "name":"Calculator",
+      "responsive":true,
+      "website":"https://calculator-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/calculator",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":true,
+         "JS":true,
+         "REACT":false,
+         "SASS":false
+      }
+   },
+   {
+      "id":23,
+      "name":"Pepsi Landing Page",
+      "responsive":false,
+      "website":"https://pepsi-landing-page-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/pepsi-landing-page",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":true,
+         "JS":true,
+         "REACT":false,
+         "SASS":false
+      }
+   },
+  
+   {
+      "id":24,
+      "name":"New Year Countdown",
+      "responsive":false,
+      "website":"https://new-year-countdown-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/new-year-countdown",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":true,
+         "JS":true,
+         "REACT":false,
+         "SASS":false
+      }
+   },
+   {
+      "id":25,
+      "name":"Clock",
+      "responsive":true,
+      "website":"https://clock-d.netlify.app/",
+      "repository":"https://github.com/banicaDaniel07/clock",
+      "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+      "code":{
+         "HTML":true,
+         "CSS":true,
+         "JS":true,
+         "REACT":false,
+         "SASS":false
+      }
+   },
+   
 ]
 
 export default DATA_JSON;
+
+// {
+//    "id":1,
+//    "name":"False ALL",
+//    "responsive":false,
+//    "website":"https://mulan-landing-page-d.netlify.app/",
+//    "repository":"https://github.com/banicaDaniel07/mulan-landing-page",
+//    "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+//    "code":{
+//       "HTML":false,
+//       "CSS":false,
+//       "JS":false,
+//       "REACT":false,
+//       "SASS":false
+//    }
+// }
+
+
+// {
+//    "id":1,
+//    "name":"HERE",
+//    "responsive":false,
+//    "website":"https://HERE.netlify.app/",
+//    "repository":"https://github.com/banicaDaniel07/HERE",
+//    "cover": "https://i.ibb.co/DGqW2jr/Mulan-landing-page.png",
+//    "code":{
+//       "HTML":false,
+//       "CSS":false,
+//       "JS":false,
+//       "REACT":false,
+//       "SASS":false
+//    }
+// },
--- src/pages/projects/projects.page.jsx
+++ src/pages/projects/projects.page.jsx
@@ -1,34 +1,99 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from '../../components/card/card.component';
 import DATA_JSON from '../../data';
 
 import './projects.styles.scss';
 
-class ProjectsPage extends Component {
-    constructor(){
-        super();
+const ProjectsPage = () => {
+        const [projects] = useState(DATA_JSON);
+        const [filtred, setFiltred] = useState(projects);
+        const [html, setHtml] = useState(false);
+        const [css, setCss] = useState(false);
+        const [js, setJs] = useState(false);
+        const [sass, setSass] = useState(false);
+        const [react, setReact] = useState(false);
 
-        this.state  = {
-            projects: DATA_JSON
-        }
-    }
+        // o metoda care ia proiectele si verifica ce e on si filtreaza
 
-    render(){
-        const {projects} = this.state;
+
+        // CODE IS WOKING BUT YELLOW WORNING APEARS
+
+
+
+        // const filterProjects =  () => {
+        //     let newFilterArr = projects;
+        //     if(html === true){
+        //         newFilterArr = newFilterArr.filter(elem => elem.code.HTML === true);
+        //     }
+        //     if(css === true){
+        //         newFilterArr = newFilterArr.filter(elem => elem.code.CSS === true);
+        //     }
+        //     if(js === true){
+        //         newFilterArr = newFilterArr.filter(elem => elem.code.JS === true);
+        //     }
+        //     if(sass === true){
+        //         newFilterArr = newFilterArr.filter(elem => elem.code.SASS === true);
+        //     }
+        //     if(react === true){
+        //         newFilterArr = newFilterArr.filter(elem => elem.code.REACT === true);
+        //     }
+        //     setFiltred(newFilterArr);
+        // }
+
+        // useEffect( () => {filterProjects()},[html, css, js, sass, react] )
+
+        const renderHtml =  () => {
+             setHtml(!html);
+        }    
+        const renderCss =  () => {
+            setCss(!css);
+        }    
+        const renderJs = () => {
+            setJs(!js);
+        }    
+        const renderSass = () => {
+            setSass(!sass);
+        }    
+        const renderReact = () => {
+            setReact(!react);
+        }    
+
+        //  So i moved the function in use effect and no more yellow working
+        
+        useEffect(()=>{
+            let newFilterArr = projects;
+            if(html === true){
+                newFilterArr = newFilterArr.filter(elem => elem.code.HTML === true);
+            }
+            if(css === true){
+                newFilterArr = newFilterArr.filter(elem => elem.code.CSS === true);
+            }
+            if(js === true){
+                newFilterArr = newFilterArr.filter(elem => elem.code.JS === true);
+            }
+            if(sass === true){
+                newFilterArr = newFilterArr.filter(elem => elem.code.SASS === true);
+            }
+            if(react === true){
+                newFilterArr = newFilterArr.filter(elem => elem.code.REACT === true);
+            }
+            setFiltred(newFilterArr)},[html, css, js, sass, react, projects])
+
+
         return (
             <div className='projects'>
                 <h1 className="projects__title">My Projects</h1>
                 <div className="projects__container">
                     <div className="projects__options">
-                        <div className="option__button">HTML</div>
-                        <div className="option__button">CSS</div>
-                        <div className="option__button">JS</div>
-                        <div className="option__button">SASS</div>
-                        <div className="option__button">REACT</div>
+                        <div onClick={renderHtml} className={`option__button ${html ? "button__active" : ""}`}>HTML</div>
+                        <div onClick={renderCss} className={`option__button ${css ? "button__active" : ""}`}>CSS</div>
+                        <div onClick={renderJs} className={`option__button ${js ? "button__active" : ""}`}>JS</div>
+                        <div onClick={renderSass} className={`option__button ${sass ? "button__active" : ""}`}>SASS</div>
+                        <div onClick={renderReact} className={`option__button ${react ? "button__active" : ""}`}>REACT</div>
                     </div>
                     <div className="projects__card">
                         {
-                            projects.map(project => {
+                            filtred.map(project => {
                                 return (
                                     <Card  key={project.id} project={project}/>
                                 )
@@ -39,6 +104,6 @@ class ProjectsPage extends Component {
             </div>
         )
     }
-}
+
 
 export default ProjectsPage
